78e20637f22c6fd17a47219d32d71fdf9b32c374
brunno ronaldo <brunnoronaldo18@gmail.com>
2026-07-21 20:15:15 -0300

    chore: update last updated date in patient_generator.py and time_simulator.py

--- src/simulator/tools/time_simulator.py
+++ src/simulator/tools/time_simulator.py
@@ -1,13 +1,13 @@
 # @author: Brunno Ronaldo
 # @created: 2026-07-10
-# @last updated: 2026-07-10
+# @last updated: 2026-07-21
 # @version: 0.5.0
 
 from datetime import datetime, timedelta
 
 class SimulationTime:
     # Atributo de classe (compartilhado)
-    simulated_data = datetime(2026, 1, 1)
+    simulated_data = datetime.now()
 
     @staticmethod
     def advance_time(): # we access this method to advance the simulation time by one day with "Name"
